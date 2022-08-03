bff3a924eeda642884a59029e2e51ad85fa031a3
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2022-08-04 00:27:44 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_d2_tip_calculator
+ 
---
+++ android/app/src/main/kotlin/com/example/flutter_d2_tip_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_d2_tip_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
